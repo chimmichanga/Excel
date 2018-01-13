f26577858a9803239420e5b88c48f06de70ea7a0
chimmichanga <sk1265@scarletmail.rutgers.edu>
2018-01-13 00:56:13 -0500

    update

---
+++ excel automation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 17:56:43 2018
+
+@author: Shivam
+"""
+
+import openpyxl
+import os
+
+os.chdir('C:\\Users\\Shivam\\Documents')
+
+wb=openpyxl.load_workbook('test.xlsx')
+
+sheet=wb.get_sheet_by_name('test1')
+
+
+data=[]
+for i in range(1,5):
+    value=(sheet.cell(row=i,column=3).value)
+    data.append(value)
+
+print(data)
+
+text_file=open(os.path.join('C:\\Users\\Shivam\\Documents',"file.txt"), 'a')   
+text_file.write(str(data))
+text_file.close()
+
+  
